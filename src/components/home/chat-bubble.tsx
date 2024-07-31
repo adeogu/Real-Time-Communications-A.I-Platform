8be6572b069a3a22dc898ef0d76bea799966bce7
adeogu <adeogu1234@gmail.com>
2024-07-31 18:20:49 +0100

    9-kick users

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
 import ReactPlayer from "react-player";
-// import ChatAvatarActions from "./chat-avatar-actions";
+import ChatAvatarActions from "./chat-avatar-actions";
 // import { Bot } from "lucide-react";
 
 type ChatBubbleProps = {
@@ -57,6 +57,11 @@ const ChatBubble = ({me,message, previousMessage}: ChatBubbleProps) => {
 					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
 
 						<OtherMessageIndicator />
+
+						{<ChatAvatarActions message={message} me={me} />} 
+
+
+
 						{renderMessageContent()}
 						{open && <ImageDialog src={message.content} open={open} onClose={() => setOpen(false)} />}
 						<MessageTime time={time} fromMe={fromMe} />
@@ -67,11 +72,9 @@ const ChatBubble = ({me,message, previousMessage}: ChatBubbleProps) => {
 
 
 
-
 {/* 
 						{/* {!fromAI && <OtherMessageIndicator />}
-						{fromAI && <Bot size={16} className='absolute bottom-[2px] left-2' />}
-						{<ChatAvatarActions message={message} me={me} />} */}
+						{fromAI && <Bot size={16} className='absolute bottom-[2px] left-2' />} */}
 					
 					</div>
 				</div>
@@ -85,6 +88,14 @@ const ChatBubble = ({me,message, previousMessage}: ChatBubbleProps) => {
 			<div className='flex gap-1 w-2/3 ml-auto'>
 				<div className={`flex  z-20 max-w-fit px-2 pt-1 rounded-md shadow-md ml-auto relative ${bgClass}`}>
 					<SelfMessageIndicator />
+
+
+
+
+
+
+
+
 					{renderMessageContent()}
 					{open && <ImageDialog src={message.content} open={open} onClose={() => setOpen(false)} />}
 					<MessageTime time={time} fromMe={fromMe} />
